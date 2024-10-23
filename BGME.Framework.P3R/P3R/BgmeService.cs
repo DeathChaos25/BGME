44cc80b094d45475ba9d95206e53f6c759b68f32
RyoTune <zutomayofan50@gmail.com>
2024-10-23 04:42:45 -0500

    Add BGME for P3R.

---
+++ BGME.Framework.P3R/P3R/BgmeService.cs
@@ -0,0 +1,24 @@
+﻿using BGME.Framework.Music;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using Ryo.Interfaces;
+
+namespace BGME.Framework.P3R.P3R;
+
+internal class BgmeService : IBgmeService
+{
+    private readonly EncounterBgm encounterBgm;
+    private readonly Sound bgm;
+
+    public BgmeService(ICriAtomEx criAtomEx, MusicService music)
+    {
+        this.bgm = new(criAtomEx, music);
+        this.encounterBgm = new(music);
+    }
+
+    public void SetVictoryDisabled(bool isDisabled)
+    {
+        this.bgm.SetVictoryDisabled(isDisabled);
+        this.encounterBgm.SetVictoryDisabled(isDisabled);
+    }
+}
