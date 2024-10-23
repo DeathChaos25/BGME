c9ca2fcf3d7f9ee7d37e45322d71a9c32e06b1dd
RyoTune <zutomayofan50@gmail.com>
2024-10-23 00:32:31 -0500

    Add PersonaMusicScript types project.

---
+++ PersonaMusicScript/PersonaMusicScript.Types/Games/GameMusic.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonaMusicScript.Types.Games;
+
+public class GameMusic
+{
+    public Game Game { get; set; }
+
+    public int Version { get; set; }
+
+    public string DefaultBaseReplacementPath { get; set; } = string.Empty;
+
+    public string? DefaultEncoder { get; set; }
+
+    public List<Song> Songs { get; set; } = new();
+}
---
+++ PersonaMusicScript/PersonaMusicScript.Types/Games/Song.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonaMusicScript.Types.Games;
+
+public class Song
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public string[] Tags { get; set; } = Array.Empty<string>();
+
+    public int CueId { get; set; }
+
+    public string? ReplacementPath { get; set; }
+
+    public string? Encoder { get; set; }
+}
